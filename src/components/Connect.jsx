4ac0252686cd9865640b85922b77afc26f7f0bdb
FROSTYLAN <castillo089frosty@gmail.com>
2022-04-21 00:56:39 -0500

    create styles and connect with components

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Connect.css';
 
 const Connect = () => {
     return (
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Work.css';
 
 const Work = () => {
     return (
